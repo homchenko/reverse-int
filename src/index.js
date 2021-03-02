44523096a5b6ae75d95a6c10d06b5152f0afad43
homchenko <posharkow@mail.ru>
2021-03-02 19:31:29 +0300

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let num = Math.abs(n);
+    let nnn = num.toString().split('');
+    let arr = [];
+    for(let i = nnn.length-1; i >= 0; i--){
+      arr.push(nnn[i]);
+    } 
+    let str = Number(arr.join(''));
+    return (str);
 }
